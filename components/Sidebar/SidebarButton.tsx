02e0edb74ab7efb61f58a67a2c6b4e975fd3e911
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-06-23 12:53:52 +0530

    Update SidebarButton.tsx

--- components/Sidebar/SidebarButton.tsx
+++ components/Sidebar/SidebarButton.tsx
@@ -8,7 +8,6 @@ interface Props {
 
 export const SidebarButton: FC<Props> = ({ text, icon, onClick }) => {
   return (
-<p>Put your ChimeraAPI Key in OpenAI API Key section</p>
     <button
       className="flex w-full cursor-pointer select-none items-center gap-3 rounded-md py-3 px-3 text-[14px] leading-3 text-white transition-colors duration-200 hover:bg-gray-500/10"
       onClick={onClick}

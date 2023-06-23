191da0b0f2b6bbfe94c6cee60d82bf9982b0b7fe
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-06-23 12:41:38 +0530

    Update const.ts

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -9,7 +9,7 @@ export const DEFAULT_TEMPERATURE =
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
 
 export const OPENAI_API_TYPE =
-  process.env.OPENAI_API_TYPE || 'Chimera';
+  process.env.OPENAI_API_TYPE || 'openai';
 
 export const OPENAI_API_VERSION =
   process.env.OPENAI_API_VERSION || '2023-03-15-preview';

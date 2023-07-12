e46abcd20a928ac2bc415d1fac93ebb2c8a9a93a
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-07-12 12:08:58 +0530

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -8,77 +8,98 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_0613 = 'gpt-3.5-turbo-0613',
-  GPT_3_5_16K = 'gpt-3.5-turbo-16k',
-  GPT_3_5_16K_0613 = 'gpt-3.5-turbo-16k-0613',
-  GPT_4 = 'gpt-4',
+  GPT_3_5_TURBO = 'gpt-3.5-turbo',
+  GPT_3_5_TURBO_0613 = 'gpt-3.5-turbo-0613',
+  GPT_3_5_TURBO_16K = 'gpt-3.5-turbo-16k',
+  GPT_3_5_TURBO_16K_POE = 'gpt-3.5-turbo-16k-poe',
+  GPT_3_5_TURBO_16K_0613 = 'gpt-3.5-turbo-16k-0613',
   GPT_4_0613 = 'gpt-4-0613',
+  GPT_4 = 'gpt-4',
+  GPT_4_POE = 'gpt-4-poe',
+  GPT_4_32K_0613 = 'gpt-4-32k-0613',
   GPT_4_32K = 'gpt-4-32k',
-  CLAUDE_PLUS = 'claude+',
-  CLAUDE_INSTANT = 'claude-instant',
+  GPT_4_32K_POE = 'gpt-4-32k-poe',
+  CLAUDE_2_100K = 'claude-2-100k',
   CLAUDE_INSTANT_100K = 'claude-instant-100k',
-  BARD = 'bard',
-  CHAT_BISON_001 = 'chat-bison-001', // Added the new model id
+  CLAUDE_INSTANT = 'claude-instant',
+  CHAT_BISON_001 = 'chat-bison-001',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.CHAT_BISON_001;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
+  [OpenAIModelID.GPT_3_5_TURBO]: {
+    id: OpenAIModelID.GPT_3_5_TURBO,
+    name: 'GPT-3.5 Turbo',
+    maxLength: 30000,
     tokenLimit: 4096,
   },
-  [OpenAIModelID.GPT_3_5_0613]: {
-    id: OpenAIModelID.GPT_3_5_0613,
-    name: 'GPT-3.5-0613',
-    maxLength: 12000,
+  [OpenAIModelID.GPT_3_5_TURBO_0613]: {
+    id: OpenAIModelID.GPT_3_5_TURBO_0613,
+    name: 'GPT-3.5 Turbo 0613',
+    maxLength: 30000,
     tokenLimit: 4096,
   },
-  [OpenAIModelID.GPT_3_5_16K]: {
-    id: OpenAIModelID.GPT_3_5_16K,
-    name: 'GPT-3.5-16K',
-    maxLength: 48000,
+  [OpenAIModelID.GPT_3_5_TURBO_16K]: {
+    id: OpenAIModelID.GPT_3_5_TURBO_16K,
+    name: 'GPT-3.5 Turbo 16K',
+    maxLength: 30000,
     tokenLimit: 16384,
   },
-  [OpenAIModelID.GPT_3_5_16K_0613]: {
-    id: OpenAIModelID.GPT_3_5_16K_0613,
-    name: 'GPT-3.5-16K-0613',
-    maxLength: 48000,
+  [OpenAIModelID.GPT_3_5_TURBO_16K_POE]: {
+    id: OpenAIModelID.GPT_3_5_TURBO_16K_POE,
+    name: 'GPT-3.5 Turbo 16K POE',
+    maxLength: 30000,
     tokenLimit: 16384,
+  },
+  [OpenAIModelID.GPT_3_5_TURBO_16K_0613]: {
+    id: OpenAIModelID.GPT_3_5_TURBO_16K_0613,
+    name: 'GPT-3.5 Turbo 16K 0613',
+    maxLength: 30000,
+    tokenLimit: 16384,
+  },
+  [OpenAIModelID.GPT_4_0613]: {
+    id: OpenAIModelID.GPT_4_0613,
+    name: 'GPT-4 0613',
+    maxLength: 30000,
+    tokenLimit: 8192,
   },
   [OpenAIModelID.GPT_4]: {
     id: OpenAIModelID.GPT_4,
     name: 'GPT-4',
-    maxLength: 24000,
+    maxLength: 30000,
     tokenLimit: 8192,
   },
-  [OpenAIModelID.GPT_4_0613]: {
-    id: OpenAIModelID.GPT_4_0613,
-    name: 'GPT-4-0613',
-    maxLength: 24000,
+  [OpenAIModelID.GPT_4_POE]: {
+    id: OpenAIModelID.GPT_4_POE,
+    name: 'GPT-4 POE',
+    maxLength: 30000,
     tokenLimit: 8192,
+  },
+  [OpenAIModelID.GPT_4_32K_0613]: {
+    id: OpenAIModelID.GPT_4_32K_0613,
+    name: 'GPT-4 32K 0613',
+    maxLength: 30000,
+    tokenLimit: 32768,
   },
   [OpenAIModelID.GPT_4_32K]: {
     id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
+    name: 'GPT-4 32K',
+    maxLength: 30000,
     tokenLimit: 32768,
   },
-  [OpenAIModelID.CLAUDE_PLUS]: {
-    id: OpenAIModelID.CLAUDE_PLUS,
-    name: 'Claude+',
+  [OpenAIModelID.GPT_4_32K_POE]: {
+    id: OpenAIModelID.GPT_4_32K_POE,
+    name: 'GPT-4 32K POE',
     maxLength: 30000,
-    tokenLimit: 10240,
+    tokenLimit: 32768,
   },
-  [OpenAIModelID.CLAUDE_INSTANT]: {
-    id: OpenAIModelID.CLAUDE_INSTANT,
-    name: 'Claude Instant',
-    maxLength: 30000,
-    tokenLimit: 10240,
+  [OpenAIModelID.CLAUDE_2_100K]: {
+    id: OpenAIModelID.CLAUDE_2_100K,
+    name: 'Claude 2 100K',
+    maxLength: 300000,
+    tokenLimit: 102400,
   },
   [OpenAIModelID.CLAUDE_INSTANT_100K]: {
     id: OpenAIModelID.CLAUDE_INSTANT_100K,
@@ -86,16 +107,17 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 300000,
     tokenLimit: 102400,
   },
-  [OpenAIModelID.BARD]: {
-    id: OpenAIModelID.BARD,
-    name: 'Bard',
+  [OpenAIModelID.CLAUDE_INSTANT]: {
+    id: OpenAIModelID.CLAUDE_INSTANT,
+    name: 'Claude Instant',
     maxLength: 30000,
     tokenLimit: 10240,
   },
-  [OpenAIModelID.CHAT_BISON_001]: { // New model entry
+  [OpenAIModelID.CHAT_BISON_001]: {
     id: OpenAIModelID.CHAT_BISON_001,
-    name: 'Chat Bison 001', // Change the name as desired
+    name: 'Chat Bison 001',
     maxLength: 30000,
     tokenLimit: 10240,
   },
 };
+  

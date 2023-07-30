8ba07304eabd78957a4537339e5e9b9e5282f1ad
hihumanzone <101509991+hihumanzone@users.noreply.github.com>
2023-07-30 12:20:39 +0530

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -11,10 +11,12 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
   GPT_3_5_0613 = 'gpt-3.5-turbo-0613',
+  GPT_3_5_0301 = 'gpt-3.5-turbo-0301',
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_3_5_16K_POE = 'gpt-3.5-turbo-16k-poe',
   GPT_3_5_16K_0613 = 'gpt-3.5-turbo-16k-0613',
   GPT_4_0613 = 'gpt-4-0613',
+  GPT_4_0314 = 'gpt-4-0314',
   GPT_4 = 'gpt-4',
   GPT_4_POE = 'gpt-4-poe',
   GPT_4_32K_0613 = 'gpt-4-32k-0613',
